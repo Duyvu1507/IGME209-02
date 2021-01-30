d668e5cab60541a478b0204d338627ef4016848d
Duy Vu <dv8618@rit.edu>
2021-01-30 15:06:25 -0500

    final commit

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -2,32 +2,27 @@
 //
 
 #include <iostream>
+#include <iomanip>
 
 int main()
 {
     std::cout << "Hello World!\n" << std::endl;
 
+    // Seconds in December
     int seconds;
     seconds = 31 * 24 * 60 * 60;
 
+    // Area of a circile with radius 6.2
     double area;
     area = 3.1415 * (pow(6.2, 2));
 
+    // How integer division works
     double division;
     division = 11 / 3;
 
     std::cout << "There are " << seconds << " seconds in December" << std::endl;
-    std::cout << "The area of a circle with a radius 6.2 is " << area << std::endl;
+    std::cout << "The area of a circle with a radius 6.2 is " << std::setprecision(4) << area << std::endl;
     std::cout << "With integer division, the number is always rounded down ie: 11 / 3 gives us " << division << std::endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
