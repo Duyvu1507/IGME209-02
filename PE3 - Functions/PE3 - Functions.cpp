4ae356bf8c3d90493f95b674114c8a21bf439f56
Duy Vu <dv8618@rit.edu>
2021-02-02 08:15:17 -0500

    Initial Commit PE-3 Functions

---
+++ PE3 - Functions/PE3 - Functions.cpp
@@ -0,0 +1,16 @@
+// PE3 - Functions.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "functions.h"
+
+int main()
+{
+    PrintName();
+}
+
+// Function that prints out my name on a new line
+void PrintName( ) {
+    std::cout << "\nDuy Vu" << std::endl;
+}
+
