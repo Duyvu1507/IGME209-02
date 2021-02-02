160b2d36e5a9bc3ff8e539c7e6ae6968f293fff3
Duy Vu <dv8618@rit.edu>
2021-02-02 09:24:56 -0500

    PE4 - C Strings Intial Commit

---
+++ PE4 - C Strings/PE4 - C Strings.cpp
@@ -0,0 +1,28 @@
+// PE4 - C Strings.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    // Define first C string and print the length
+    char word[100] = "supercalifraglistic";
+    std::cout << strlen(word) << std::endl;
+
+    // Define second C string and append it to first and prints the result
+    char word2[100] = "expialidocious";
+    strcat_s(word, word2);
+    std::cout << word << std::endl;
+
+    // Finds how many times the letter i appears in the combined string
+    int count = 0;
+    for (char i : word) {
+        if (i == 'i') {
+            count++;
+        }
+    }
+    std::cout << "The letter i appears " << count << " times in the combined string" << std::endl;
+
+    
+}
+
