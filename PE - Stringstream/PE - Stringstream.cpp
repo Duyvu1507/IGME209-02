97585d87d78a11d77a06d7f2b4c75ab950cd8c7c
Duy Vu <dv8618@rit.edu>
2021-03-27 11:11:57 -0400

    final commit (PE stringstream)

--- PE - Stringstream/PE - Stringstream.cpp
+++ PE - Stringstream/PE - Stringstream.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main()
 {
     int currentMonth = 3;
-    int currentDay = 25;
+    int currentDay = 27;
     int daysInEachMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
     char info[90] = "Duy Vu 7 / 15";
 
@@ -21,22 +21,25 @@ int main()
     char slash;
 
     ss >> firstName >> lastName >> birthMonth >> slash >> birthDay;
-    
-    int daysTillBirthday = 0;
 
-    while (currentMonth != birthMonth and currentDay != birthDay) {
-        daysTillBirthday += 1;
+    int daysTillBday = 0;
+    bool cont = true;
+    while (cont == true) {
+        if (currentMonth == birthMonth && currentDay == birthDay) {
+            cont = false;
+        }
+        daysTillBday += 1;
         currentDay += 1;
-
-        if (currentDay > daysInEachMonth[currentMonth]) {
+        if (currentDay > daysInEachMonth[currentMonth - 1]) {
             currentDay = 1;
             currentMonth += 1;
-            if (currentMonth == 13) {
-                currentMonth = 1;
-            }
+        }
+        if (currentMonth == 13) {
+            currentMonth = 1;
         }
     }
 
-    cout << firstName << ", Your birthday is in " << daysTillBirthday << " day(s)" << endl;
+    cout << firstName << ", your birthday is in " << daysTillBday << " day(s)" << endl;
+    
 }
 
